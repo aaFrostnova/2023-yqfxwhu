d862e2dca330338d3ffebdae726e071bd406316b
qyang <qyangshaw@163.com>
2023-12-04 18:27:46 +0800

    url setting changed

--- demo/urls.py
+++ demo/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('demo/<int:num>/', views.hello),
-    path('demo/index/', views.index),
-    path('demo/graph/', views.graph),
+    path('index/', views.index),
+    path('graph/', views.graph),
 ]
