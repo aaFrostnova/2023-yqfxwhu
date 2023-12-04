b8a9086d9163f728bcd2dd6346e745651488ad19
qyang <qyangshaw@163.com>
2023-12-04 19:12:21 +0800

    urls setting changed

--- demo/urls.py
+++ demo/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index),
+    # 访问127.0.0.1:8000则会显示views.index
+    path('', views.index),
+    # 访问127.0.0.1:8000则会显示views.graph
     path('graph/', views.graph),
 ]
