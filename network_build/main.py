f4f1e3700c292dbae585031b5276cefe74caf6a0
xiaoqiyang <qyangshaw@163.com>
2023-12-13 16:07:06 +0800

    main update

--- network_build/main.py
+++ network_build/main.py
@@ -1,4 +1,4 @@
-from data_process import senator_reader, analysis_reader, data_reader, init_matrix
+from data_process import senator_reader, analysis_reader, data_reader, init_matrix, build_matrix
 from network_dump import network_dump, relation_dump
 import csv
 import random
@@ -6,16 +6,9 @@ import random
 file1 = r".\network_build\following.csv"
 file2 = r".\network_build\coat.csv"
 file3 = r".\network_build\coexist.csv"
-senator_info       = senator_reader()            # 参议员所有的信息
+following_mat, coat_mat, coexist_mat, senator_info, _, _, _, _ = build_matrix(file1, file2, file3)
 analysis_info      = analysis_reader()
-following_dict     = data_reader(filename=file1) # 关注者字典
-coat_dict          = data_reader(filename=file2)
-coexist_dict       = data_reader(filename=file3)
-senator_node_vocab = list(senator_info.keys())                                 # 参议院节点的名字 
-following_mat      = init_matrix(following_dict, senator_node_vocab) # 关注者矩阵
-num_senator_nodes  = len(senator_node_vocab)
-coat_mat           = init_matrix(coat_dict, senator_node_vocab)
-coexist_mat        = init_matrix(coexist_dict, senator_node_vocab)
+
 
 _file1 = r".\static\data\following-network.json"
 _file2 = r".\static\data\coat-network.json"
